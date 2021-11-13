11d4aa5166da828db6c5b1bdafe86549cc4538ef
umarcor <unai.martinezcorral@ehu.eus>
2021-11-13 05:34:28 +0100

    utils/setup: cleanup

--- utils/setup.sh
+++ utils/setup.sh
@@ -25,27 +25,21 @@ cd $(dirname "$0")
 
 pip3 install -r pyHDLC/requirements.txt
 
-HDL_ARCH="${HDL_ARCH:-$1}"
-
-if [ -n "$CI" ]; then
-  echo "$(pwd)/bin" >> $GITHUB_PATH
-  echo "PYTHONPATH=$(pwd)" >> $GITHUB_ENV
-
-  if [ -n "$HDL_ARCH" ]; then
-    unset _arch
-    case $HDL_ARCH in
-      arm32v7) _arch="arm";;
-      arm64v8) _arch="aarch64";;
-      ppc64le|s390x|riscv64) _arch="$HDL_ARCH";;
-    esac
-    if [ -n "$_arch" ]; then
-      docker run --rm --privileged aptman/qus -s -- -p $_arch
-    fi
-  fi
-
-else
-
+if [ -z "$CI" ]; then
   export PATH="$PATH:$(pwd)/bin"
   export PYTHONPATH="$(pwd)"
+  exit 0
+fi
 
+echo "$(pwd)/bin" >> $GITHUB_PATH
+echo "PYTHONPATH=$(pwd)" >> $GITHUB_ENV
+
+unset _arch
+case $1 in
+  arm32v7) _arch="arm";;
+  arm64v8) _arch="aarch64";;
+  ppc64le|s390x|riscv64) _arch="$1";;
+esac
+if [ -n "$_arch" ]; then
+  docker run --rm --privileged aptman/qus -s -- -p $_arch
 fi
