9f346460af54f3a6eaefba22762f6f99a4fc0dcc
Unai Martinez-Corral <umartinezcorral@antmicro.com>
2022-11-08 03:43:47 +0100

    utils/pyHDLC: update copyright header

--- utils/pyHDLC/__init__.py
+++ utils/pyHDLC/__init__.py
@@ -2,6 +2,7 @@
 #   Unai Martinez-Corral
 #
 # Copyright 2021-2022 Unai Martinez-Corral <unai.martinezcorral@ehu.eus>
+# Copyright 2022 Unai Martinez-Corral <umartinezcorral@antmicro.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
